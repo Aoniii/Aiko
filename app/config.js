cbd17ff6f948d4f06ebd80c57adb070091f97658
Aoniii <aoniii.contact@gmail.com>
2023-12-20 16:55:11 +0100

    change prefix

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     app: {
-        px: '!',
+        px: '~',
         token: 'MTE4NzA0MDIyMzMyMjk4MDM5Mw.GwhU1W.XC2UMx11tAicnEqm8OHAag1ETcwjBBi-fpdZrs',
         playing: ""
     },
