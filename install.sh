b149a5c8c945bbbac5cddd39648f81b5aef83137
Aoniii <aoniii.contact@gmail.com>
2023-12-20 20:17:42 +0100

    x

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install make -y
 apt-get install curl -y
-apt-get install mysql-server -y
+apt-get install mariadb-server -y
 apt-get install python3 -y
 apt-get install pip -y
 curl -fsSL https://get.docker.com | sh
