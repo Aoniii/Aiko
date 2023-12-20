3c9f1eaa233c7c20e7187db828d394758e696f6b
Aoniii <aoniii.contact@gmail.com>
2023-12-20 20:06:47 +0100

    add mysql

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install make -y
 apt-get install curl -y
+apt-get install mysql-server -y
 apt-get install python3 -y
 curl -fsSL https://get.docker.com | sh
 
