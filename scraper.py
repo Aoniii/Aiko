3c6f36f1546c6aad7609928f1b5b956f2d51acce
Aoniii <aoniii.contact@gmail.com>
2023-12-21 15:58:42 +0100

    x

--- scraper.py
+++ scraper.py
@@ -11,21 +11,21 @@ if response.ok:
 	soup = BeautifulSoup(response.text, 'lxml')
 	champs = soup.findAll('a',  class_="style__Wrapper-sc-n3ovyt-0 style__ResponsiveWrapper-sc-n3ovyt-4 jMmGcS iUmzcQ style__Item-sc-13btjky-3 idPziT isVisible isFirstTime")
 	for champ in champs:
-		print("INSERT INTO users (name, img, nameP, nameQ, nameW, nameE, nameR, imgP, imgQ, imgW, imgE, imgR) VALUES (\"", end="")
-		print(champ.find('span', class_="style__Text-sc-n3ovyt-3 kThhiV").text, end="")
-		print("\", \"", end="")
+		print("INSERT INTO users (name, img, nameP, nameQ, nameW, nameE, nameR, imgP, imgQ, imgW, imgE, imgR) VALUES ('", end="")
+		print(champ.find('span', class_="style__Text-sc-n3ovyt-3 kThhiV").text.replace("'", "''"), end="")
+		print("', '", end="")
 		print(champ.find('img')['src'], end="")
 		response2 = requests.get('https://www.leagueoflegends.com/' + champ['href'])
 		if response2.ok:
 			soup2 = BeautifulSoup(response2.text, 'lxml')
 			comps_name = soup2.findAll('h5')
 			for comp_name in comps_name:
-				print("\", \"", end="")
-				print(comp_name.text, end="")
+				print("', '", end="")
+				print(comp_name.text.replace("'", "''"), end="")
 			comps_img = soup2.findAll('span', class_="style__OptionIconContent-sc-1ac4kmt-6 eQgsrb")
 			for comp_img in comps_img:
-				print("\", \"", end="")
+				print("', '", end="")
 				print(comp_img.find('img')['src'], end="")
-		print("\");")
+		print("');")
 
 print('\nSELECT * FROM users;')
